327d463724b38aca4eb7ea18d5ef1270a1463690
cleo.hsu <kayahino@gmail.com>
2020-02-06 11:22:53 +0800

    project init
    1. 安裝 axios 及 vue 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueCompositionApi)
+Vue.use(VueAxios, axios)
 
 new Vue({
   render: h => h(App)

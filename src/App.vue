d616052ea95cf52d0fa91a1803a38b01d9fb583c
cleo.hsu <kayahino@gmail.com>
2020-02-06 11:50:04 +0800

    axios 載入 data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { onMounted, reactive } from '@vue/composition-api'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  setup (props, { root }) {
+    onMounted(async () => {
+      const { data } = await root.$http.get('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json')
+      state.maskData = data.features
+      state.isLoaded = true
+    })
+
+    const state = reactive({
+      isLoaded: false,
+      maskData: null
+    })
+
+    return {
+      state
+    }
   }
 }
 </script>

d3bb3100d26e676be9d1ff1c4d11a547ae742885
cleo.hsu <kayahino@gmail.com>
2020-02-09 02:58:57 +0800

    Add 一些功能function

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,11 +1,52 @@
 import 'leaflet/dist/leaflet.css'
 import $L from 'leaflet'
 
+const geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: '#ff7800',
+  color: '#000',
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+}
+
 const createMap = (divId, options) => {
   let map = $L.map(divId, options)
   return map
 }
 
+const createTileLayer = async (map, url, options) => {
+  let tileLayer = await $L.tileLayer(url, options)
+  tileLayer.addTo(map)
+  return tileLayer
+}
+
+const pinMark = (map, data) => {
+  $L
+    .geoJSON(data, {
+      pointToLayer: (feature, latlng) => {
+        return $L.circleMarker(latlng, geojsonMarkerOptions)
+      }
+    })
+    .bindPopup((layer) => {
+      return layer.feature.properties.name
+    })
+    .addTo(map)
+}
+
+const showDistanceRange = (map, latlng) => {
+  const circle = $L.circleMarker(latlng, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+  }).addTo(map)
+  return circle
+}
+
 export default {
-  createMap
+  createMap,
+  createTileLayer,
+  pinMark,
+  showDistanceRange
 }
